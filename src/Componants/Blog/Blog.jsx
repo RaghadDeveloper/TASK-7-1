cce717a0c8b1479222638764bea22280b41fc841
RaghadDeveloper <raghadmh620@gmail.com>
2025-03-08 17:47:48 +0300

    Navbar bg

--- src/Componants/Blog/Blog.jsx
+++ src/Componants/Blog/Blog.jsx
@@ -1,61 +1,67 @@
-import './Blog.css'
-import React from 'react'
+import "./Blog.css";
+import React from "react";
 import { TbArrowNarrowRight } from "react-icons/tb";
-import LeftTitle from '../LeftTitle/LeftTitle';
-import BlogCard from '../BlogCard/BlogCard';
-import img1 from '../../assets/images/blogThumb1_2.webp'
-import img2 from '../../assets/images/blogThumb1_3.webp'
-import img3 from '../../assets/images/blogThumb1_1.webp'
-import img from '../../assets/images/ctaThumb.webp'
+import LeftTitle from "../LeftTitle/LeftTitle";
+import BlogCard from "../BlogCard/BlogCard";
+import img1 from "../../assets/images/blogThumb1_2.webp";
+import img2 from "../../assets/images/blogThumb1_3.webp";
+import img3 from "../../assets/images/blogThumb1_1.webp";
+import img from "../../assets/images/ctaThumb.webp";
 
 export default function Blog() {
-
   const data = [
     {
-      'img': img1,
-      'date':'MARCH 14,2024',
-      'title':'Best And Fastest Data Server Ever',
-      'profileImg':'./../../src/assets/images/blogProfile1_2.webp',
-      'name':'Kristin Watson',
-      'desc':'Web Designer'
+      img: img1,
+      date: "MARCH 14,2024",
+      title: "Best And Fastest Data Server Ever",
+      profileImg: "./../../src/assets/images/blogProfile1_2.webp",
+      name: "Kristin Watson",
+      desc: "Web Designer",
     },
     {
-      'img': img2,
-      'date':'MARCH 14,2024',
-      'title':'Best And Fastest Data Server Ever',
-      'profileImg':'./../../src/assets/images/blogProfile1_3.webp',
-      'name':'Kristin Watson',
-      'desc':'Web Designer'
+      img: img2,
+      date: "MARCH 14,2024",
+      title: "Best And Fastest Data Server Ever",
+      profileImg: "./../../src/assets/images/blogProfile1_3.webp",
+      name: "Kristin Watson",
+      desc: "Web Designer",
     },
     {
-      'img': img3,
-      'date':'MARCH 14,2024',
-      'title':'Best And Fastest Data Server Ever',
-      'profileImg':'./../../src/assets/images/blogProfile1_1.webp',
-      'name':'Kristin Watson',
-      'desc':'Web Designer'
-    }
-  ]
+      img: img3,
+      date: "MARCH 14,2024",
+      title: "Best And Fastest Data Server Ever",
+      profileImg: "./../../src/assets/images/blogProfile1_1.webp",
+      name: "Kristin Watson",
+      desc: "Web Designer",
+    },
+  ];
   return (
-    <section className='py-5 position-relative'>
-      <LeftTitle title = {'BLOG & NEWS'} text = {'Featured News And Insights'} />
+    <section className="py-5 position-relative">
+      <LeftTitle title={"BLOG & NEWS"} text={"Featured News And Insights"} />
 
-      <div className='container my-5 d-flex flex-wrap justify-content-center gap-5'>
-        
-        {
-          data.map((item, index) => (
-            <BlogCard img={item.img} date={item.date} title={item.title} profileImg={item.profileImg} name={item.name} desc={item.desc} />
-          ))
-        }
-
+      <div className="container my-5 d-flex flex-wrap justify-content-center gap-5">
+        {data.map((item, index) => (
+          <BlogCard
+            key={index}
+            img={item.img}
+            date={item.date}
+            title={item.title}
+            profileImg={item.profileImg}
+            name={item.name}
+            desc={item.desc}
+          />
+        ))}
       </div>
 
-      <section className='home-footer my-5 p-5 d-flex flex-wrap justify-content-end align-items-center gap-5 row-gap-1 position-absolute'>
-          <img src={img} alt="" className='d-none d-lg-block'/>
-          <p className='fs-3 text-light mx-5'>Stay Connected With Cutting Edge IT</p>
-          <button className='rounded-pill px-4 py-2 mx-5'>TALK TO A SPECIALIST <TbArrowNarrowRight /></button>
+      <section className="home-footer my-5 p-5 d-flex flex-wrap justify-content-end align-items-center gap-5 row-gap-1 position-absolute">
+        <img src={img} alt="" className="d-none d-lg-block" />
+        <p className="fs-3 text-light mx-5">
+          Stay Connected With Cutting Edge IT
+        </p>
+        <button className="rounded-pill px-4 py-2 mx-5">
+          TALK TO A SPECIALIST <TbArrowNarrowRight />
+        </button>
       </section>
-
     </section>
-  )
+  );
 }
--- src/Componants/Navbar/Navbar.jsx
+++ src/Componants/Navbar/Navbar.jsx
@@ -1,27 +1,60 @@
-import './Navbar.css'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Button from 'react-bootstrap/Button';
+import "./Navbar.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
 import { CiSearch } from "react-icons/ci";
 import { FaArrowRight } from "react-icons/fa6";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa";
-import logo from '../../assets/images/logo.svg'
+import logo from "../../assets/images/logo.svg";
+import { useEffect, useState } from "react";
 
-function r_Navbar() {
+function RNavbar() {
   const navigate = useNavigate();
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <Navbar collapseOnSelect expand="lg" className="w-100 bg-body-tertiary m-2 px-3 z-3 nav position-fixed top-0">
-      <Container className='d-flex justify-content-between'>
-        <Navbar.Brand href="/"><img src={logo} alt="logo" width={'150px'} /></Navbar.Brand>
-        <div className='d-flex align-items-center gap-3'>
-          <CiSearch className='search-icon fs-3 d-block' />
-          <FaShoppingCart className='shop-icon fs-3 d-block primary-color' />
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" className='border-none' />
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      className={`${
+        scrolled ? "bg-scroll" : ""
+      } w-100 bg-body-tertiary m-2 px-3 z-3 nav position-fixed top-0`}
+    >
+      <Container className="d-flex justify-content-between">
+        <Navbar.Brand href="/">
+          <img src={logo} alt="logo" width={"150px"} />
+        </Navbar.Brand>
+        <div className="d-flex align-items-center gap-3">
+          <CiSearch className="search-icon fs-3 d-block" />
+          <FaShoppingCart className="shop-icon fs-3 d-block primary-color" />
+          <Navbar.Toggle
+            aria-controls="responsive-navbar-nav"
+            className="border-none"
+          />
         </div>
-        <Navbar.Collapse id="responsive-navbar-nav" className='d-lg-flex justify-content-between'>
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          className="d-lg-flex justify-content-between"
+        >
           <Nav className="m-auto align-items-center">
             <NavDropdown title="Home" id="collapsible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -34,9 +67,7 @@ function r_Navbar() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link
-              onClick={() => navigate("/About")}
-            >About</Nav.Link>
+            <Nav.Link onClick={() => navigate("/About")}>About</Nav.Link>
             <NavDropdown title="Services" id="collapsible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -70,20 +101,19 @@ function r_Navbar() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link
-              onClick={() => navigate("/Contact")}>Contact</Nav.Link>
+            <Nav.Link onClick={() => navigate("/Contact")}>Contact</Nav.Link>
           </Nav>
-          <Nav className='menu-end d-flex justify-content-center align-items-center gap-3'>
-            <CiSearch className='search-icon fs-3' />
-            <FaShoppingCart className='shop-icon fs-3 primary-color' />
-            <Button className='nav-btn rounded-pill d-flex justify-content-center align-items-center gap-2 px-3'>GET A QUOTE <FaArrowRight /> </Button>
+          <Nav className="menu-end d-flex justify-content-center align-items-center gap-3">
+            <CiSearch className="search-icon fs-3" />
+            <FaShoppingCart className="shop-icon fs-3 primary-color" />
+            <Button className="nav-btn rounded-pill d-flex justify-content-center align-items-center gap-2 px-3">
+              GET A QUOTE <FaArrowRight />{" "}
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
-
     </Navbar>
-
   );
 }
 
-export default r_Navbar;
+export default RNavbar;
